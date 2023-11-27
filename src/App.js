6544832b1fa107c030b7597184ff08789f2c2394
¨Claudio <claudio.romano@jolivi.com.br>
2023-11-27 16:47:00 -0300

    feature: criando componente Button

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import  React, { useState } from "react";
+import Button from "./component/Button";
 
 function App() {
+
+  function testando(){
+    return (console.log('teste de componente'))
+  }
+
   return (
     <div className="App">
+      <Button onClick={testando} label="Novo Ensinamento" />
     </div>
   );
 }
