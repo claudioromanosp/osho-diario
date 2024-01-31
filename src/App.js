5c2287c8625da7ac31cdac318f31c664914f0844
¨Claudio <claudio.romano@jolivi.com.br>
2024-01-31 14:27:52 -0300

    Feature: adicionando Vercel Analytics

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import  React from "react";
+import { Analytics } from "@vercel/analytics/react";
+import React from "react";
 import Phrases from "./components/Phrases";
 
 function App() {
   return (
     <div className="App">
       <Phrases />
+      <Analytics />
     </div>
   );
 }
